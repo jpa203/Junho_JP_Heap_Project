a291c6054dc7031d22b2812db29a1013c1dfbe3b
Junho Eum <jxe282@gmail.com>
2023-06-29 15:22:09 -0400

    Update lsa.py

--- main_code/lsa.py
+++ main_code/lsa.py
@@ -17,6 +17,7 @@ def extract_topic_docx(source_dir,num_topics=3):
     terms = vectorizer.get_feature_names_out()
     num_top_words = 5 # Number of top words to display for each topic
 
+    print("Top 5 keywords for each document"+"\n"+"__________________________")
     for i, component in enumerate(lsa.components_):
         top_words = [terms[index] for index in component.argsort()[:-num_top_words - 1:-1]]
         print(f'Topic {i+1}: {",".join(top_words)}')
@@ -31,9 +32,10 @@ def extract_topic_pdf(pdf_token,num_topics=3):
     num_top_words = 7 # Number of top words to display for each topic
 
     topics = []
+    
     for i, component in enumerate(lsa.components_):
         top_words = [terms[index] for index in component.argsort()[:-num_top_words - 1:-1]]
-        print(f'Topic {i+1}: {",".join(top_words)}')
+        print(f'Topic words: {",".join(top_words)}')
         topic_word = "_".join(top_words)
     return topic_word
     
