6b95ef20d0c3b6293901d7cd32b3873e06f2d5a8
Junho Eum <jxe282@gmail.com>
2023-06-29 15:25:56 -0400

    Update main.py

--- main_code/main.py
+++ main_code/main.py
@@ -47,6 +47,6 @@ def heap_main(source_dir):
     draw_best_heap(best_heap)
 
 if __name__ == "__main__":
-    source_dir = "/Users/junhoeum/Desktop/Summer_23/doc_clustering_algo/test_document_3_27_23/test_sample"
+    source_dir = "YOUR_SOURCE_PATH"
     K_means_main(source_dir)
     heap_main(source_dir)
