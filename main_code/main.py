b7db5acb4bd39788fbf257236f8d264855ba36b3
Junho Eum <jxe282@gmail.com>
2023-07-04 14:05:50 -0700

    Add empirical time complexity function main.py

--- main_code/main.py
+++ main_code/main.py
@@ -4,7 +4,8 @@ warnings.filterwarnings('ignore')
 from matplotlib.axes._axes import _log as matplotlib_axes_logger
 matplotlib_axes_logger.setLevel('ERROR')
 from draw_heap import  draw_best_heap
-from K_means_cluster import elbow_method, plot_k_means
+from K_means_cluster import elbow_method, plot_k_means, K_means_cluster
+from sklearn.cluster import KMeans
 
 """
 main function for K-means clustering
@@ -51,17 +52,61 @@ def heap_main(source_dir):
     silhouete_score = cluster_object.compute_silhouette_score(labels, reduced_vectors)
 
     reduced_vectors = cluster_object.perform_pca(best_heap_lst)
-    print(len(reduced_vectors))
-    print(reduced_vectors)
+    print("length of the reduced vector from PCA:",len(reduced_vectors))
+    print("Reduced vectors to 2-d space: \n",reduced_vectors)
 
     silhouete_score = cluster_object.compute_silhouette_score(labels, reduced_vectors)
 
-    print(silhouete_score)
+    print("silhouete coeff score:",silhouete_score)
 
+# Main function for emprical time complexity test
+def main_heap_algo_time_test(source_dir):
+    # Initialize document_clf object
+    cluster_object = document_clf(source_dir)
+
+    # Create tf-idf matrix for source directory
+    cluster_object.create_tf_idf_matrix()
+
+    # Get a best single heap based on avg cosine similarity score
+    best_single_heap = cluster_object.get_best_heap_cosine()
+
+    return best_single_heap
     
+def main_Kmeans_algo_time_test(source_dir):
+    # Initialize document_clf object
+    cluster_object = document_clf(source_dir)
+
+    # Create tf-idf matrix for source directory
+    cluster_object.create_tf_idf_matrix()
+
+    doc_lst = cluster_object.get_document_names()
+
+    # after finding the best number of clusters, use K-means clustering
+    kmeans = KMeans(n_clusters=4, random_state=23)
+    kmeans.fit(cluster_object.tf_idf_matrix)
+    cluster_labels = kmeans.predict(cluster_object.tf_idf_matrix)
+
+    doc_label_dict = dict()
+    for i in range(len(doc_lst)):
+        # Assign labels to each document based on cluster labels
+        doc_label_dict[doc_lst[i]] = cluster_labels[i]
+    
+    return doc_label_dict
 
 if __name__ == "__main__":
-    source_dir = "/Users/junhoeum/Desktop/Summer_23/doc_clustering_algo/test_document_3_27_23/test_sample"
-    K_means_main(source_dir)
-    heap_main(source_dir)
+    source_dir = "SOURCE_DIRECTORY_PATH"
+
+    # main function to run the code
+    # K_means_main(source_dir)
+    # heap_main(source_dir)
+
+    # main function for time complexity analysis
+    kmeans_result = main_Kmeans_algo_time_test(source_dir)
+    best_heap_cluster = main_heap_algo_time_test(source_dir)
+    
+    # Dictionary mapped with cluster number and documents
+    print(kmeans_result)
+    draw_best_heap(best_heap_cluster)
+
+
 
