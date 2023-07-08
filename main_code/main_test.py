4f60f59a679bd2919dd11cad6f21edfa3366011e
Junho Eum <jxe282@gmail.com>
2023-07-08 17:30:45 -0400

    Update main_test.py

--- main_code/main_test.py
+++ main_code/main_test.py
@@ -4,7 +4,7 @@ warnings.filterwarnings('ignore')
 from matplotlib.axes._axes import _log as matplotlib_axes_logger
 matplotlib_axes_logger.setLevel('ERROR')
 from draw_heap import  draw_best_heap
-from K_means_cluster import elbow_method, plot_k_means, K_means_cluster
+from K_means_cluster import elbow_method, plot_k_means, K_means_cluster, elbow_method_test
 from sklearn.cluster import KMeans
 
 """
