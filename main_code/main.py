0f0ed15b07134bbbcbf057ff03ccca16d102ca83
Junho Eum <jxe282@gmail.com>
2023-07-07 20:20:56 -0400

    add test_k_means_main() to fix user input error main.py

--- main_code/main.py
+++ main_code/main.py
@@ -24,6 +24,22 @@ def K_means_main(source_dir):
     plot_k_means(cluster_object.tf_idf_matrix,cluster_object.term, source_dir, cluster_count)
 
 """
+run this function to check the user input error from the tkinter library
+"""
+def test_k_means_main(source_dir):
+    # Initialize document_clf object
+    cluster_object = document_clf(source_dir)
+
+    # Create tf-idf matrix for source directory
+    cluster_object.create_tf_idf_matrix()
+
+    #plot elbow method to decide the number of clusters for K_means_clustering
+    cluster_count = elbow_method_test(cluster_object.tf_idf_matrix)
+    
+    #After inertia decrease point from elbow method, set new num of clusters and get the wordcloud
+    plot_k_means(cluster_object.tf_idf_matrix,cluster_object.term, source_dir, cluster_count)
+
+"""
 main function for heap-based clustering
 """
 def heap_main(source_dir):
@@ -100,8 +116,9 @@ if __name__ == "__main__":
     source_dir = "./test_data"
 
     # main function to run the code
-    K_means_main(source_dir)
-    heap_main(source_dir)
+    # TODO: Run this code and check for the input error
+    test_k_means_main(source_dir)
+    #heap_main(source_dir)
 
     # main function for time complexity analysis
     # kmeans_result = main_Kmeans_algo_time_test(source_dir)
